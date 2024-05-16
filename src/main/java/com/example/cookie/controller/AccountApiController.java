44d19aba25eb61f2e2221b12e34ccbc434877a26
fullm <matoma1_@naver.com>
2024-05-16 16:32:01 +0900

    Feat: 헤더를 통한 인증

--- src/main/java/com/example/cookie/controller/AccountApiController.java
+++ src/main/java/com/example/cookie/controller/AccountApiController.java
@@ -4,6 +4,7 @@ import com.example.cookie.model.LoginRequest;
 import com.example.cookie.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +19,9 @@ public class AccountApiController {
     private final UserService userService;
 
     @PostMapping("/login")
-    public String login (@RequestBody LoginRequest loginRequest, HttpServletResponse httpServletResponse){
+    public String login (@RequestBody LoginRequest loginRequest,
+                         HttpServletResponse httpServletResponse,
+                         HttpSession httpSession){ //JSESSIONID
 
         return userService.login(loginRequest, httpServletResponse);
     }
